9d00b46d9fa9855f35d84ffbb1311969f0e3a373
Shazil Hamzah <shazil.hamzah@outlook.com>
2024-11-29 14:26:01 +0500

    relaigned the grid + subroutines

--- project.asm
+++ project.asm
@@ -24,8 +24,29 @@ row6: dw '3','4',' ',' ','7','1','2','5','9',0
 row7: dw '1','6','4','8','9','5','3','2','7',0
 row8: dw '5','8','3','7','2',' ',' ','9','6',0
 row9: dw '2','7',' ','3','1','6',' ','8','5',0
-rowAddress: dw 0
 
+mrow1: dw '8',' ','6','4',' ','9','5',' ',' ',0
+mrow2: dw '9',' ',' ','2','6','7','8','4','3',0
+mrow3: dw '4','3','7','1','5',' ',' ','6','2',0
+mrow4: dw '6',' ',' ','9','4','2','7',' ','8',0
+mrow5: dw '7','9','2','5',' ','3','6','1','4',0
+mrow6: dw '3','4',' ',' ','7',' ','2','5','9',0
+mrow7: dw '1','6','4','8','9','5','3','2','7',0
+mrow8: dw ' ','8','3','7','2',' ',' ',' ','6',0
+mrow9: dw '2','7',' ','3','1','6',' ','8',' ',0
+
+hrow1: dw '8','2','6','4',' ','9','5',' ',' ',0
+hrow2: dw '9',' ','5',' ',' ',' ','8','4','3',0
+hrow3: dw '4','3','7',' ','5',' ',' ','6','2',0
+hrow4: dw '6',' ','1','9','4',' ','7',' ',' ',0
+hrow5: dw '7',' ','2','5',' ',' ','6','1','4',0
+hrow6: dw '3','4',' ',' ','7','1','2','5','9',0
+hrow7: dw ' ','6',' ',' ','9','5','3','2',' ',0
+hrow8: dw '5','8',' ',' ','2',' ',' ','9','6',0
+hrow9: dw ' ','7',' ','3','1',' ',' ','8','5',0
+
+rowAddress: dw 0
+difficulty: dw 0
 ; SCORE SCREEN PRINTING
 time: dw 'Time: 00:00',0
 scoreString: dw 'Score: 0',0
@@ -41,8 +62,8 @@ printstr:
     push cx
     push si
     push di
-    mov ax, 0xb800
-    mov es, ax ; point es to video base
+    ; mov ax, 0xb900
+    ; mov es, ax ; point es to video base
     mov al, 80 ; load al with columns per row
     mul byte [bp+10] ; multiply with y position
     add ax, [bp+12] ; add x position
@@ -110,6 +131,8 @@ printColsSubroutine:
         mov ax, emptyString      ; Address of emptyString
         push ax                  ; Push the string address
         push word [length]   ; Push the length of the empty string
+        mov ax, 0xb800
+        mov es, ax ; point es to video base
         call printstr            ; Call the printstr subroutine
         jmp nextColumn           ; Jump to handle the next column
 
@@ -118,6 +141,8 @@ printColsSubroutine:
         mov ax, verticalLine     ; Address of verticalLine
         push ax                  ; Push the string address
         push word [length]       ; Push the length of verticalLine
+        mov ax, 0xb800
+        mov es, ax ; point es to video base
         call printstr            ; Call the printstr subroutine
 
     nextColumn:
@@ -128,6 +153,59 @@ printColsSubroutine:
         add cx, 8                ; Increment x position (next column block)
         cmp cx, 81               ; Compare for the end of screen width (80 columns)
         jle printCols            ; If not yet, loop back to print next column block
+        ret
+
+printRemainingColsSubroutine:
+    mov cx, 3
+    printColsRemain:
+        mov bx, 0
+    loopColRemain:
+        mov ax, cx
+        push ax                 ; push x position
+        mov ax, bx
+        push ax                 ; push y position
+
+        ; Check if cx is 27 or 51 for blue attribute and empty string, else use yellow
+        cmp cx, 27
+        je handleBlueEmptyStringRemain    ; If cx is 27, jump to handle blue and empty string
+        cmp cx, 51
+        je handleBlueEmptyStringRemain    ; If cx is 51, jump to handle blue and empty string
+
+        ; Default case: use yellow attribute and verticalLine
+    yellowAttributeRemain:
+        mov ax, 0x0E             ; Yellow on black attribute
+        jmp pushVerticalLineRemain     ; Jump to push verticalLine
+
+    handleBlueEmptyStringRemain:
+        mov ax, 0x70             ; Blue on black attribute
+        push ax                  ; Push the attribute
+
+        ; Push emptyString
+        mov ax, emptyString      ; Address of emptyString
+        push ax                  ; Push the string address
+        push word [length]   ; Push the length of the empty string
+        mov ax, 0xbA00
+        mov es, ax ; point es to video base
+        call printstr            ; Call the printstr subroutine
+        jmp nextColumnRemain           ; Jump to handle the next column
+
+    pushVerticalLineRemain:
+        push ax                  ; Push the attribute
+        mov ax, verticalLine     ; Address of verticalLine
+        push ax                  ; Push the string address
+        push word [length]       ; Push the length of verticalLine
+        mov ax, 0xbA00
+        mov es, ax ; point es to video base
+        call printstr            ; Call the printstr subroutine
+
+    nextColumnRemain:
+        inc bx                   ; Increment y position
+        cmp bx, 12               ; Compare for the end of screen (25 rows)
+        jle loopColRemain              ; If not yet, loop back to print next row
+
+        add cx, 8                ; Increment x position (next column block)
+        cmp cx, 81               ; Compare for the end of screen width (80 columns)
+        jle printColsRemain            ; If not yet, loop back to print next column block
         ret
 
 printRowsSubroutine:
@@ -159,6 +237,8 @@ printRowsSubroutine:
         mov ax, emptyString      ; Address of emptyString
         push ax                 ; Push the string address
         push word [length]  ; Push the length of the empty string
+        mov ax, 0xb800
+        mov es, ax ; point es to video base
         call printstr           ; Call the printstr subroutine
         jmp nextRow             ; Jump to handle the next row
 
@@ -167,6 +247,8 @@ printRowsSubroutine:
         mov ax, horizontalLine  ; Address of horizontalLine
         push ax                 ; Push the string address
         push word [length]      ; Push the length of horizontalLine
+        mov ax, 0xb800
+        mov es, ax ; point es to video base
         call printstr           ; Call the printstr subroutine
 
     nextRow:
@@ -174,9 +256,62 @@ printRowsSubroutine:
         cmp cx, 75              ; Compare for the end of row width (75 is arbitrary, set for spacing)
         jle loopRow             ; If not yet, loop back to print next column
 
-        add bx, 3               ; Increment y position (next row)
+        add bx, 4              ; Increment y position (next row)
         cmp bx, 24              ; Compare for the end of screen (25 rows)
         jle printRows           ; If not yet, loop back to print next row
+        ret
+
+printRowsSubroutineRemain:
+    mov bx, 0               ; Start at row 0
+    printRowsRemain:
+        mov cx, 3               ; Start at column 3
+    loopRowRemain:
+        mov ax, cx
+        push ax                 ; Push x (column) position
+        mov ax, bx
+        push ax                 ; Push y (row) position
+
+        ; Check if bx is 9 or 18 for blue attribute and empty string
+        cmp bx, 9
+        je handleBlueEmptyString1Remain  ; If bx is 9, jump to handle blue and empty string
+        cmp bx, 18
+        je handleBlueEmptyString1Remain  ; If bx is 18, jump to handle blue and empty string
+
+        ; Default case: use yellow attribute and horizontalLine
+    yellowAttribute1Remain:
+        mov ax, 0x0E            ; Yellow on black attribute
+        jmp pushHorizontalLine1Remain ; Jump to push horizontalLine
+
+    handleBlueEmptyString1Remain:
+        mov ax, 0x7F            ; Blue on black attribute
+        push ax                 ; Push the attribute
+
+        ; Push emptyString
+        mov ax, emptyString      ; Address of emptyString
+        push ax                 ; Push the string address
+        push word [length]  ; Push the length of the empty string
+        mov ax, 0xbA00
+        mov es, ax ; point es to video base
+        call printstr           ; Call the printstr subroutine
+        jmp nextRowRemain             ; Jump to handle the next row
+
+    pushHorizontalLine1Remain:
+        push ax                 ; Push the attribute
+        mov ax, horizontalLine  ; Address of horizontalLine
+        push ax                 ; Push the string address
+        push word [length]      ; Push the length of horizontalLine
+        mov ax, 0xbA00
+        mov es, ax ; point es to video base
+        call printstr           ; Call the printstr subroutine
+
+    nextRowRemain:
+        add cx, 1               ; Increment column position (cx)
+        cmp cx, 75              ; Compare for the end of row width (75 is arbitrary, set for spacing)
+        jle loopRowRemain             ; If not yet, loop back to print next column
+
+        add bx, 4              ; Increment y position (next row)
+        cmp bx, 12              ; Compare for the end of screen (25 rows)
+        jle printRowsRemain           ; If not yet, loop back to print next row
         ret
 
 printBorder:
@@ -191,6 +326,8 @@ printBorder:
         mov ax, emptyString
         push ax ; push address of message
         push word [length] ; push message length
+        mov ax, 0xb800
+        mov es, ax ; point es to video base
         call printstr ; call the printstr subroutine
         inc cx
         cmp cx,75
@@ -207,6 +344,8 @@ printBorder:
         mov ax, emptyString
         push ax ; push address of message
         push word [length] ; push message length
+        mov ax, 0xb800
+        mov es, ax ; point es to video base
         call printstr ; call the printstr subroutine
         inc cx
         cmp cx,75
@@ -223,6 +362,8 @@ printBorder:
         mov ax, emptyString
         push ax ; push address of message
         push word [length] ; push message length
+        mov ax, 0xb800
+        mov es, ax ; point es to video base
         call printstr ; call the printstr subroutine
         inc cx
         cmp cx,25
@@ -239,10 +380,86 @@ printBorder:
         mov ax, emptyString
         push ax ; push address of message
         push word [length] ; push message length
+        mov ax, 0xb800
+        mov es, ax ; point es to video base
         call printstr ; call the printstr subroutine
         inc cx
         cmp cx,25
         jle colRight
+    ret
+
+printBorderRemain:
+    mov cx,3
+    rowTopRemain:
+        mov ax, cx
+        push ax ; push x position
+        mov ax, 0
+        push ax ; push y position
+        mov ax, 0x40 
+        push ax ; push attribute
+        mov ax, emptyString
+        push ax ; push address of message
+        push word [length] ; push message length
+        mov ax, 0xbA00
+        mov es, ax ; point es to video base
+        call printstr ; call the printstr subroutine
+        inc cx
+        cmp cx,75
+        jle rowTopRemain
+
+    mov cx,3
+    rowBottomRemain:
+        mov ax, cx
+        push ax ; push x position
+        mov ax, 12
+        push ax ; push y position
+        mov ax, 0x40
+        push ax ; push attribute
+        mov ax, emptyString
+        push ax ; push address of message
+        push word [length] ; push message length
+        mov ax, 0xbA00
+        mov es, ax ; point es to video base
+        call printstr ; call the printstr subroutine
+        inc cx
+        cmp cx,75
+        jle rowBottomRemain
+    
+    mov cx,0
+    colLeftRemain:
+        mov ax, 3
+        push ax ; push x position
+        mov ax, cx
+        push ax ; push y position
+        mov ax, 0x40
+        push ax ; push attribute
+        mov ax, emptyString
+        push ax ; push address of message
+        push word [length] ; push message length
+        mov ax, 0xbA00
+        mov es, ax ; point es to video base
+        call printstr ; call the printstr subroutine
+        inc cx
+        cmp cx,12
+        jle colLeftRemain
+
+    mov cx,0
+    colRightRemain:
+        mov ax, 75
+        push ax ; push x position
+        mov ax, cx
+        push ax ; push y position
+        mov ax, 0x40
+        push ax ; push attribute
+        mov ax, emptyString
+        push ax ; push address of message
+        push word [length] ; push message length
+        mov ax, 0xbA00
+        mov es, ax ; point es to video base
+        call printstr ; call the printstr subroutine
+        inc cx
+        cmp cx,12
+        jle colRightRemain
     ret
 
 printNumbers:
@@ -263,19 +480,21 @@ printNumbers:
         add si, bx
         push si          ; push address of message
         push word 2      ; push message length
+        mov ax, 0xb800
+        mov es, ax ; point es to video base
         call printstr    ; call the printstr subroutine
         add bx, 2
         add dx, 8
         cmp bx, 18
         jle printInnerLoop
 
-        add cx, 3        ; Move to next row
+        add cx, 4        ; Move to next row
         add word [rowAddress], 20  ; Move to next row's data
         cmp cx, 29
         jle printOuterLoop
         ret
 
-systemPauseSubroutine:
+askForInput:
     mov ah,00h
     int 16h
     ret
@@ -315,20 +534,20 @@ nextloc:
     pop es
     ret
 
-clrscr_page1:
+clrscr_page3:
     push es            ; Save ES register
     push ax            ; Save AX register
     push di            ; Save DI register
 
     mov ax, 0xb800     ; Set segment to video memory
     mov es, ax         ; ES = 0xB800 (video memory segment)
-    mov di, 0x1000     ; Start at offset 0x1000 (beginning of page 1)
+    mov di, 0x2000     ; Start at offset 0x1000 (beginning of page 1)
 
-nextloc_page1:
+nextloc_page3:
     mov word [es:di], 0x0720  ; Store space character (0x20) with attribute (0x07)
     add di, 2                 ; Move to the next character position (2 bytes per character)
     cmp di, 0x2000            ; 0x2000 = 4000 in hex, end of page 1 memory
-    jne nextloc_page1         ; If not done, keep clearing
+    jne nextloc_page3         ; If not done, keep clearing
 
     pop di            ; Restore DI register
     pop ax            ; Restore AX register
@@ -553,32 +772,48 @@ toPage1Subroutine:
     mov ax,0501h
     int 10h
     ret
+    
+toPage2Subroutine:
+    mov ax,0502h
+    int 10h
+    ret
+
+toPage3Subroutine:
+    mov ax,0503h
+    int 10h
+    ret
 
 timerPrintSubroutine:
-    call clrscr_page1
-    mov ah, 0x13 ; service 13 - print string
-    mov al, 1 ; subservice 01 – update cursor
-    mov bh, 1 ; output on page 0
-    mov bl, 7 ; normal attrib
-    mov dx, 0x0B24 ; row 11 column 36
-    mov cx, 11 ; length of string
-    push cs
-    pop es ; segment of string
-    mov bp, time ; offset of string
-    int 0x10 ; call BIOS video service
+    call clrscr_page3
+    mov ax,36
+    push ax
+    mov ax,11
+    push ax
+    mov ax,7
+    push ax
+    mov ax,time
+    push ax
+    mov ax,11
+    push ax
+    mov ax,0xbb00
+    mov es,ax
+    call printstr
 
 scorePrintSubroutine:
-    mov ah, 0x13 ; service 13 - print string
-    mov al, 1 ; subservice 01 – update cursor
-    mov bh, 1 ; output on page 0
-    mov bl, 7 ; normal attrib
-    mov dx, 0x0C26 ; row 12 column 38
-    mov cx, 8 ; length of string
-    push cs
-    pop es ; segment of string
-    mov bp, scoreString ; offset of string
-    int 0x10 ; call BIOS video service
-    call toPage1Subroutine
+    mov ax,38
+    push ax
+    mov ax,12
+    push ax
+    mov ax,7
+    push ax
+    mov ax,scoreString
+    push ax
+    mov ax,8
+    push ax
+    mov ax,0xbb00
+    mov es,ax
+    call printstr
+    call toPage3Subroutine
     ret
 
 gameSubroutine:
@@ -590,20 +825,31 @@ gameSubroutine:
         call turnoffspeakers
         call display_message_effect  
         call display_continue_msg 
-        call systemPauseSubroutine
+        call askForInput
 
-    ; GAME GRID
-    GAMEGRID:
+    ; GAME CONFIGURATIONS
+    SETTINGS:
         call clrscr 
         call settingVideoModeSubroutine
         call blackBackgroundSubroutine
+
+    ; GRID PRINTING
+    GAMEBOARD:
         call printRowsSubroutine
+        call printRowsSubroutineRemain
         call printColsSubroutine
+        call printRemainingColsSubroutine
         call printBorder
+        call printBorderRemain
         call printNumbers
-        call systemPauseSubroutine
+
+    ; MECHANICS
+    MECHANICS:
+        call askForInput
+        call toPage2Subroutine
+        call askForInput
         call timerPrintSubroutine
-        call systemPauseSubroutine
+        call askForInput
         call toPage0Subroutine
 
     ; END SCREEN
